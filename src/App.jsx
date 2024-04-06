46b7e66e6c62cda8ae9eac5991a0988d200f3c60
YelyzavetaMala <malaelizaveta678@gmail.com>
2024-04-06 17:51:05 +0300

    task

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,25 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import ContactForm from './components/ContactForm';
 import SearchBox from './components/SearchBox';
 import ContactList from './components/ContactList';
-import { fetchContacts, addContact, deleteContact } from './redux/contactsOps';
+import { fetchContacts } from './redux/contactsOps';
 import { useEffect } from 'react';
-import { changeFilter, selectFilteredContacts } from './redux/filtersSlice';
+
 import './App.css';
 
 function App() {
   const dispatch = useDispatch();
-  const filter = useSelector(state => state.filters.name);
 
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
 
-  const handleAddContact = newContact => {
-    dispatch(addContact(newContact));
-  };
-
-  const handleDeleteContact = contactId => {
-    dispatch(deleteContact(contactId));
-  };
-
-  const filteredContacts = useSelector(state => selectFilteredContacts(state)); 
-
-  const handleSearch = value => {
-    dispatch(changeFilter(value));
-  };
-
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm onAdd={handleAddContact} />
-      <SearchBox value={filter} onSearch={handleSearch} />
-      <ContactList contacts={filteredContacts} onDelete={handleDeleteContact} />
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
     </div>
   );
 }
